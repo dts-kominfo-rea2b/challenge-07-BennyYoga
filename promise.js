1448d9471a25fea2efc7813772c409959d8b7b3d
unknown <bennyyoga77@gmail.com>
2022-06-11 17:08:01 +0700

    Challenge 7

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const sort = (data, emo) => {
+  let x=0;
+  data.forEach(item => {
+    if (item?.hasil === emo) {
+      x+=1;
+    }
+  });
+  return x;
+}
+
+const promiseOutput = async (emosi) =>{
+  try{
+    let temp = 0;
+    const [IXX,VGC] = await Promise.all([promiseTheaterIXX(),promiseTheaterVGC()])
+    temp += sort(IXX,emosi)
+    temp += sort(VGC,emosi)
+    return temp;
+  }
+  catch(error){
+    console.log(error);
+  }
+};
+
 
 module.exports = {
   promiseOutput,
